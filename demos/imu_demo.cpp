7bdf7636ddad9cd83694f824696a703fbb3f44bb
Andrés Calderón <andres.calderon@admobilize.com>
2016-07-18 22:17:18 +0000

    IMU connected to the everloop (digital compass)

--- demos/imu_demo.cpp
+++ demos/imu_demo.cpp
@@ -3,8 +3,8 @@
  * MATRIX Labs  [http://creator.matrix.one]
  * This file is part of MATRIX Creator HAL
  *
- * MATRIX Creator HAL is free software: you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
+ * MATRIX Creator HAL is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but
@@ -18,6 +18,8 @@
 #include <unistd.h>
 #include <iostream>
 
+#include "../cpp/driver/everloop_image.h"
+#include "../cpp/driver/everloop.h"
 #include "../cpp/driver/imu_data.h"
 #include "../cpp/driver/imu_sensor.h"
 #include "../cpp/driver/wishbone_bus.h"
@@ -31,13 +33,35 @@ int main() {
   hal::IMUSensor imu_sensor;
   imu_sensor.Setup(bus);
 
+  hal::Everloop everloop;
+  everloop.Setup(bus);
+
+  hal::EverloopImage image1d;
+
   hal::IMUData imu_data;
 
   while (true) {
+    for (hal::LedValue& led : image1d.leds) {
+      led.red = 0;
+      led.white = 0;
+    }
+
     imu_sensor.Read(&imu_data);
 
-    std::cout << "yaw = " << imu_data.yaw << std::endl;
-    std::cout << "roll = " << imu_data.roll << std::endl;
+    int pin = imu_data.yaw * 35.0 / 360.0;
+    pin = pin >= 0 ? pin : 35 - pin;
+
+    image1d.leds[pin].white = 20;
+
+    pin = (imu_data.yaw - 180.0) * 35.0 / 360.0;
+    pin = pin >= 0 ? pin : 35 - pin;
+
+    image1d.leds[pin].red = 30;
+
+    everloop.Write(&image1d);
+
+    std::cout << "yaw = " << imu_data.yaw << "\t";
+    std::cout << "roll = " << imu_data.roll << "\t";
     std::cout << "pitch = " << imu_data.pitch << std::endl;
     usleep(500000);
   }
