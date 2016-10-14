7d7d0a61784df0db47f3f1b7501db8087e00146a
Andres Calderon <andres.calderon@admobilize.com>
2016-10-14 14:28:08 -0800

    cross correlation moved to driver

--- demos/beamforming_demo.cpp
+++ demos/beamforming_demo.cpp
@@ -17,101 +17,8 @@
 #include "../cpp/driver/everloop.h"
 #include "../cpp/driver/microphone_array.h"
 #include "../cpp/driver/wishbone_bus.h"
+#include "../cpp/driver/beamforming.h"
 
-/*
-Cross-correlation between signals implemented in requency domain.
-*/
-class CrossCorrelation {
- public:
-  CrossCorrelation(int N);
-  ~CrossCorrelation();
-
-  void Exec(int16_t* a, int16_t* b);
-  float* Result();
-
- private:
-  void Corr(float* out, float* x, float* y);
-
-  int order_;
-  float* in_;
-  float* A_;
-  float* B_;
-  float* C_;
-  float* c_;
-
-  fftwf_plan forward_plan_a_;
-  fftwf_plan forward_plan_b_;
-  fftwf_plan inverse_plan_;
-};
-
-CrossCorrelation::CrossCorrelation(int N) : order_(N) {
-  /*
-  fftwf_malloc function that behave identically to malloc, except that they
-  guarantee that the returned pointer obeys any special alignment restrictions
-  imposed by any algorithm in FFTW (e.g. for SIMD acceleration).
-  */
-  in_ = (float*)fftwf_malloc(sizeof(float) * N);
-  A_ = (float*)fftwf_malloc(sizeof(float) * N);
-  B_ = (float*)fftwf_malloc(sizeof(float) * N);
-  C_ = (float*)fftwf_malloc(sizeof(float) * N);
-  c_ = (float*)fftwf_malloc(sizeof(float) * N);
-
-  forward_plan_a_ = fftwf_plan_r2r_1d(N, in_, A_, FFTW_R2HC, FFTW_ESTIMATE);
-  forward_plan_b_ = fftwf_plan_r2r_1d(N, in_, B_, FFTW_R2HC, FFTW_ESTIMATE);
-  inverse_plan_ = fftwf_plan_r2r_1d(N, C_, c_, FFTW_HC2R, FFTW_ESTIMATE);
-}
-
-CrossCorrelation::~CrossCorrelation() {
-  fftwf_destroy_plan(forward_plan_a_);
-  fftwf_destroy_plan(forward_plan_b_);
-  fftwf_destroy_plan(inverse_plan_);
-
-  fftwf_free(in_);
-  fftwf_free(A_);
-  fftwf_free(B_);
-  fftwf_free(C_);
-  fftwf_free(c_);
-}
-
-float* CrossCorrelation::Result() { return c_; }
-
-void CrossCorrelation::Exec(int16_t* a, int16_t* b) {
-  for (int i = 0; i < order_; i++) {
-    in_[i] = a[i];
-  }
-
-  fftwf_execute(forward_plan_a_);
-
-  for (int i = 0; i < order_; i++) {
-    in_[i] = b[i];
-  }
-
-  fftwf_execute(forward_plan_b_);
-
-  Corr(C_, A_, B_);
-
-  fftwf_execute(inverse_plan_);
-
-  for (int i = 0; i < order_; i++) {
-    c_[i] = c_[i] / order_;
-  }
-}
-
-void CrossCorrelation::Corr(float* out, float* x, float* y) {
-  memset(reinterpret_cast<void*>(out), 0, order_ * sizeof(float));
-
-  out[0] = x[0] * y[0];                             // r0
-  out[order_ / 2] = x[order_ / 2] * y[order_ / 2];  // r(n/2)
-
-  for (int j = 1; j < order_ / 2; j++) {
-    float a = x[j];
-    float b = x[order_ - j];
-    float c = y[j];
-    float d = -y[order_ - j];
-    out[j] += a * c - b * d;           // Re
-    out[order_ - j] += b * c + a * d;  // Im
-  }
-}
 
 namespace hal = matrix_hal;
 
@@ -135,7 +42,7 @@ int main() {
 
   int16_t buffer[mics.Channels()][mics.SamplingRate()];
 
-  CrossCorrelation corr(N);
+  hal::CrossCorrelation corr(N);
   std::valarray<float> current_mag(4);
   std::valarray<float> current_index(4);
 
