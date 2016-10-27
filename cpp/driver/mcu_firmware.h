6e1547739c0f842562e2d94d6013e1a08f4512a1
Andres Calderon <andresfcalderon@gmail.com>
2016-10-27 10:35:14 -0500

    mcu firmware handling

---
+++ cpp/driver/mcu_firmware.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2016 <Admobilize>
+ * MATRIX Labs  [http://creator.matrix.one]
+ * This file is part of MATRIX Creator HAL
+ *
+ * MATRIX Creator HAL is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CPP_DRIVER_MCU_FIRMWARE_H_
+#define CPP_DRIVER_MCU_FIRMWARE_H_
+
+#include <string>
+#include "./matrix_driver.h"
+#include "./fw_data.h"
+
+namespace matrix_hal {
+
+class MCUFirmware : public MatrixDriver {
+ public:
+  bool Read(MCUData* data);
+};
+};      // namespace matrix_hal
+#endif  // CPP_DRIVER_PRESSURE_SENSOR_H_
