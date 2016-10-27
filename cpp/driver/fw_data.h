404a97581fc497c96ae20247e5e3739646560c31
Andres Calderon <andres.calderon@admobilize.com>
2016-10-27 15:46:00 +0000

    firmware header added

---
+++ cpp/driver/fw_data.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2016 <Admobilize>
+ * MATRIX Labs  [http://creator.matrix.one]
+ * This file is part of MATRIX Creator HAL
+ *
+ * MATRIX Creator HAL is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CPP_DRIVER_MCU_DATA_H_
+#define CPP_DRIVER_MCU_DATA_H_
+
+namespace matrix_hal {
+
+class MCUData {
+ public:
+  uint32_t ID;
+  uint32_t version;
+};
+
+};      // namespace matrix_hal
+#endif  // CPP_DRIVER_MCU_DATA_H_
