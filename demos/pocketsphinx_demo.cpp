866d8afc3c9d9efe34332a1b9e29de5258ec9a2f
Antonio Vanegas <hpsaturn@gmail.com>
2017-01-11 17:12:23 -0500

    completed a refactor, remove windows compatibility

--- demos/pocketsphinx_demo.cpp
+++ demos/pocketsphinx_demo.cpp
@@ -30,22 +30,15 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
  * ====================================================================
  *
- */
-/*
  * continuous.c - Simple pocketsphinx command-line application to test
  *                both continuous listening/silence filtering from microphone
  *                and continuous file transcription.
- */
-
-/*
+ *
  * This is a simple example of pocketsphinx application that uses continuous
- * listening
- * with silence filtering to automatically segment a continuous stream of audio
- * input
- * into utterances that are then decoded.
+ * listening with silence filtering to automatically segment a continuous stream 
+ * of audio input into utterances that are then decoded.
  *
  * Remarks:
  *   - Each utterance is ended when a silence segment of at least 1 sec is
@@ -61,16 +54,8 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
-
-#if defined(_WIN32) && !defined(__CYGWIN__)
-#include <windows.h>
-#else
-#include <sys/select.h>
-#endif
-
 #include <sphinxbase/ad.h>
 #include <sphinxbase/err.h>
-
 #include "pocketsphinx.h"
 
 static const arg_t cont_args_def[] = {
@@ -85,7 +70,6 @@ static const arg_t cont_args_def[] = {
 
 static ps_decoder_t *ps_;
 static cmd_ln_t *config_;
-static FILE *rawfd_;
 
 void process_rules(const char *hyp) {
   const std::string cmd_ever = "./everloop_demo &";
@@ -140,115 +124,6 @@ void process_rules(const char *hyp) {
     system(cmd_clear.c_str());
     system(cmd_halt.c_str());
   }
-}
-
-static void print_word_times() {
-  const int frame_rate = cmd_ln_int32_r(config_, "-frate");
-  ps_seg_t *iter = ps_seg_iter(ps_);
-  while (iter != NULL) {
-    int32 sf, ef;
-    ps_seg_frames(iter, &sf, &ef);
-    const int32 pprob = ps_seg_prob(iter, NULL, NULL, NULL);
-    const float conf = logmath_exp(ps_get_logmath(ps_), pprob);
-    printf("%s %.3f %.3f %f\n", ps_seg_word(iter),
-           static_cast<float>(sf) / frame_rate,
-           static_cast<float>(ef) / frame_rate, conf);
-    iter = ps_seg_next(iter);
-  }
-}
-
-static int check_wav_header(char *header, int expected_sr) {
-  int sr;
-
-  if (header[34] != 0x10) {
-    E_ERROR("Input audio file has [%d] bits per sample instead of 16\n",
-            header[34]);
-    return 0;
-  }
-  if (header[20] != 0x1) {
-    E_ERROR("Input audio file has compression [%d] and not required PCM\n",
-            header[20]);
-    return 0;
-  }
-  if (header[22] != 0x1) {
-    E_ERROR(
-        "Input audio file has [%d] channels, expected single channel mono\n",
-        header[22]);
-    return 0;
-  }
-  sr = ((header[24] & 0xFF) | ((header[25] & 0xFF) << 8) |
-        ((header[26] & 0xFF) << 16) | ((header[27] & 0xFF) << 24));
-  if (sr != expected_sr) {
-    E_ERROR("Input audio file has sample rate [%d], but decoder expects [%d]\n",
-            sr, expected_sr);
-    return 0;
-  }
-  return 1;
-}
-
-/*
- * Continuous recognition from a file
- */
-static void recognize_from_file() {
-  int16 adbuf[2048];
-  const char *fname;
-  const char *hyp;
-  int32 k;
-  uint8 utt_started, in_speech;
-  int32 print_times = cmd_ln_boolean_r(config_, "-time");
-
-  fname = cmd_ln_str_r(config_, "-infile");
-  if ((rawfd_ = fopen(fname, "rb")) == NULL) {
-    E_FATAL_SYSTEM("Failed to open file '%s' for reading", fname);
-  }
-
-  if (strlen(fname) > 4 && strcmp(fname + strlen(fname) - 4, ".wav") == 0) {
-    char waveheader[44];
-    fread(waveheader, 1, 44, rawfd_);
-    if (!check_wav_header(waveheader,
-                          (int)cmd_ln_float32_r(config_, "-samprate")))
-      E_FATAL("Failed to process file '%s' due to format mismatch.\n", fname);
-  }
-
-  if (strlen(fname) > 4 && strcmp(fname + strlen(fname) - 4, ".mp3") == 0) {
-    E_FATAL("Can not decode mp3 files, convert input file to WAV 16kHz 16-bit "
-            "mono before decoding.\n");
-  }
-
-  ps_start_utt(ps_);
-  utt_started = FALSE;
-
-  while ((k = fread(adbuf, sizeof(int16), 2048, rawfd_)) > 0) {
-    ps_process_raw(ps_, adbuf, k, FALSE, FALSE);
-    in_speech = ps_get_in_speech(ps_);
-    if (in_speech && !utt_started) {
-      utt_started = TRUE;
-    }
-    if (!in_speech && utt_started) {
-      ps_end_utt(ps_);
-      hyp = ps_get_hyp(ps_, NULL);
-      if (hyp != NULL)
-        printf("%s\n", hyp);
-      if (print_times)
-        print_word_times();
-      fflush(stdout);
-
-      ps_start_utt(ps_);
-      utt_started = FALSE;
-    }
-  }
-  ps_end_utt(ps_);
-  if (utt_started) {
-    hyp = ps_get_hyp(ps_, NULL);
-    if (hyp != NULL) {
-      printf("%s\n", hyp);
-      if (print_times) {
-        print_word_times();
-      }
-    }
-  }
-
-  fclose(rawfd_);
 }
 
 /* Sleep for specified msec */
@@ -347,9 +222,7 @@ int main(int argc, char *argv[]) {
 
   E_INFO("%s COMPILED ON: %s, AT: %s\n\n", argv[0], __DATE__, __TIME__);
 
-  if (cmd_ln_str_r(config_, "-infile") != NULL) {
-    recognize_from_file();
-  } else if (cmd_ln_boolean_r(config_, "-inmic")) {
+  if (cmd_ln_boolean_r(config_, "-inmic")) {
     recognize_from_microphone();
   }
 
