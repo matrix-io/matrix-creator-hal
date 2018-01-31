9adac34acdf90590dec9140a9bc164250008a4c4
Andrés Calderón <andres.calderon@admobilize.com>
2018-01-31 19:52:47 +0000

    everloop anonymous thread

--- demos/micarray_recorder.cpp
+++ demos/micarray_recorder.cpp
@@ -9,6 +9,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <thread>
 #include <valarray>
 
 #include "../cpp/driver/everloop.h"
@@ -21,7 +22,7 @@ DEFINE_int32(duration, 5, "Interrupt after N seconds");
 
 namespace hal = matrix_hal;
 
-int main(int argc, char* agrv[]) {
+int main(int argc, char *agrv[]) {
   google::ParseCommandLineFlags(&argc, &agrv, true);
 
   hal::WishboneBus bus;
@@ -29,15 +30,6 @@ int main(int argc, char* agrv[]) {
 
   hal::MicrophoneArray mics;
   mics.Setup(&bus);
-
-  hal::Everloop everloop;
-  everloop.Setup(&bus);
-
-  hal::EverloopImage image;
-
-  for (auto& led : image.leds) led.red = 10;
-
-  everloop.Write(&image);
 
   int sampling_rate = FLAGS_sampling_frequency;
   int seconds_to_record = FLAGS_duration;
@@ -60,7 +52,32 @@ int main(int argc, char* agrv[]) {
     os[c].open(filename, std::ofstream::binary);
   }
 
-  uint16_t samples = 0;
+  std::thread et(
+      [seconds_to_record](hal::WishboneBus *bus) {
+
+        hal::Everloop everloop;
+        everloop.Setup(bus);
+
+        hal::EverloopImage image;
+
+        for (auto &led : image.leds)
+          led.red = 10;
+        everloop.Write(&image);
+
+        int sleep = int(1000.0 * seconds_to_record / image.leds.size());
+
+        for (auto &led : image.leds) {
+          led.red = 0;
+          led.green = 10;
+
+          everloop.Write(&image);
+
+          std::this_thread::sleep_for(std::chrono::milliseconds(sleep));
+        }
+      },
+      &bus);
+
+  uint32_t samples = 0;
   for (int s = 0; s < seconds_to_record; s++) {
     for (;;) {
       mics.Read(); /* Reading 8-mics buffer from de FPGA */
@@ -77,25 +94,15 @@ int main(int argc, char* agrv[]) {
       /* write to file */
       if (samples >= mics.SamplingRate()) {
         for (uint16_t c = 0; c < mics.Channels() + 1; c++) {
-          os[c].write((const char*)buffer[c], samples * sizeof(int16_t));
+          os[c].write((const char *)buffer[c], samples * sizeof(int16_t));
         }
         samples = 0;
         break;
       }
     }
-
-    for (int l = 0; l < int(image.leds.size() * s / (seconds_to_record)); l++) {
-      image.leds[l].red = 0;
-      image.leds[l].green = 10;
-    }
-    everloop.Write(&image);
   }
 
-  for (auto& led : image.leds) {
-    led.red = 0;
-    led.green = 10;
-  }
-  everloop.Write(&image);
+  et.join();
 
   return 0;
 }
